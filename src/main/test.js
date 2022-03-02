b2da3d881a86339a09eecd11a8592b9d8bad2c85
wang yu biao <wang775016074@126.com>
2022-03-02 23:46:26 +0800

    fix. node二进制文件

--- src/main/test.js
+++ src/main/test.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export function test(_args, Turing) {
+  Turing.Pixel_FromScreen(0, 0, 1080, 960);
+  Turing.Pixel_Preview();
   return Turing.Version();
 }
